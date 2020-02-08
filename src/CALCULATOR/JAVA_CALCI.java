e99acacb56443ce957acd170a222bcb56277ff44
karthi <karthi@192.168.0.5>
2020-02-08 21:33:36 +0530

    my calculation repo

---
+++ src/CALCULATOR/JAVA_CALCI.java
@@ -0,0 +1,39 @@
+package CALCULATOR;
+import java.util.Scanner;
+
+public class JAVA_CALCI {
+
+	public static void main(String[] args) {
+		double num1,num2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter first number:");
+		num1 = scanner.nextDouble();
+		System.out.print("Enter the second number:");
+        num2 = scanner.nextDouble();
+        System.out.print("Enter an operator(+,-,*,/):");
+        char operator = scanner.next().charAt(0);
+        scanner.close();
+        double output;
+        switch(operator)
+        {
+        case '+':
+        	output = num1 + num2;
+        	break;
+        case '-':
+        	output = num1 - num2;
+        	break;
+        case '*':
+        	output = num1 * num2;
+        	break;
+        case '/':
+        	output = num1 / num2;
+        	break;
+        default:
+        	System.out.print("You have entered wrong operator");
+        	return;
+        }
+        System.out.print(num1+" "+operator+" "+num2+":"+output);
+        
+	}
+
+}
